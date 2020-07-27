c11aace3eb7d435d97e87302e0db7531d201ae33
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-27 17:00:26 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import WaterEffect from "./WaterEffect/WaterEffect";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <WaterEffect />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <WaterEffect />
+      </div>
     </>
   );
 }
