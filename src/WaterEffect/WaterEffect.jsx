d5e52acf3dd20cb9b79347d187aca116515370b5
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-27 14:35:36 -0400

    yay:

--- src/WaterEffect/WaterEffect.jsx
+++ src/WaterEffect/WaterEffect.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class WaterEffect extends Component {
   render() {
     return (
       <>
-        <div></div>
+        <Container>
+          <Row>
+            <Col></Col>
+          </Row>
+        </Container>
       </>
     );
   }
